1fd61a749e75f4c05b9bfb5aa46b5c3f3b8ced8c
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-25 11:29:11 +0100

    Realizar iteraciones de elementos con @for

--- src/app/pages/dragon-ball/dragon-ball.ts
+++ src/app/pages/dragon-ball/dragon-ball.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+interface Character {
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragon-ball',
@@ -7,4 +12,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './dragon-ball.html',
   styleUrl: './dragon-ball.css',
 })
-export class DragonBall { }
+export class DragonBall {
+
+  characters = signal<Character[]>([
+    {
+      name: 'Goku',
+      power: 1000
+    },
+    {
+      name: 'Vegeta',
+      power: 1000
+    },
+    {
+      name: 'Piccolo',
+      power: 1000
+    }
+  ]);
+}
